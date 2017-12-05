fe7128482f774f4bea2b0c6a4dcac345e9cbd949
palpatoune <simon.btc42@gmail.com>
2017-12-05 21:45:58 +0100

    commentaire

--- polynomesSIMON.c
+++ polynomesSIMON.c
@@ -4,28 +4,33 @@
 //  IMPORTANT a chaque fois que tu veux push le projet pull le avant !//
 ////////////////////////////////////////////////////////////////////////
 
-
+/******************************************************************************/
+/* ajoutePolynomePolynome - ajoute les polynomes fourni                       */
+/*                                                                            */
+/* INPUT  : deux polynome (pointeur) a ajouter l'un a l'autre                 */
+/* OUTPUT : néant pas besoin de return car ce sont des tableaux               */
+/******************************************************************************/
 
 void ajoutePolynomePolynome(Polynome *p, Polynome *q)
 {
 
 	if (p->nb_monomes > q->nb_monomes)
 	{
-		int stop=p->nb_monomes;
+		int stop=p->nb_monomes; //valeur d'arret
 		int i;
-		for(i=0;i<=stop;i++)
+		for(i=0;i<=stop;i++) // tant que le compteur est inférieur a la valeur d'arret
 		{
 			
-			ajouteMonomePolynome(q->tab_monomes[i], p);
+			ajouteMonomePolynome(q->tab_monomes[i], p); // on ajoute le monome d'indice i du polynome q au polynome p
 		}
 	}
-	else
+	else //si le nombre de monome de q est supérieur a celui de p
 	{
-		int stop=q->nb_monomes;
+		int stop=q->nb_monomes; //valeur d'arret
 		int i;
-		for ( i = 0; i <= stop; ++i)
+		for ( i = 0; i <= stop; ++i) // tant que le compteur est inférieur a la valeur d'arret
 		{
-			ajouteMonomePolynome(p->tab_monomes[i], q);
+			ajouteMonomePolynome(p->tab_monomes[i], q); // on ajoute le monome d'indice i du polynome p au polynome q
 		}
 
 	}
