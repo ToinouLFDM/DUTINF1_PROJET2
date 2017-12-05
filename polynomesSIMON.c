49626161fbc680e1762c79def4ee265557957d4d
palpatoune <simon.btc42@gmail.com>
2017-12-05 16:26:14 +0100

    ajout de la fonction polynome polynome

--- polynomesSIMON.c
+++ polynomesSIMON.c
@@ -3,3 +3,32 @@
 ////////////////////////////////////////////////////////////////////////
 //  IMPORTANT a chaque fois que tu veux push le projet pull le avant !//
 ////////////////////////////////////////////////////////////////////////
+
+
+
+void ajoutePolynomePolynome(Polynome *p, Polynome *q)
+{
+
+	if (p->nb_monomes > q->nb_monomes)
+	{
+		int stop=p->nb_monomes;
+		int i;
+		for(i=0;i<=stop;i++)
+		{
+			
+			ajouteMonomePolynome(q->tab_monomes[i], p);
+		}
+	}
+	else
+	{
+		int stop=q->nb_monomes;
+		int i;
+		for ( i = 0; i <= stop; ++i)
+		{
+			ajouteMonomePolynome(p->tab_monomes[i], q);
+		}
+
+	}
+}
+	
+	
