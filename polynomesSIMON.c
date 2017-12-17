2e229e7843ff4ec1331a5d774b2b2cb59337fdaa
Unknown <simon.btc42@gmail.com>
2017-12-17 11:27:33 +0100

    multiplie polynome polynome
    ajoute Polynome Polynome 2

--- polynomesSIMON.c
+++ polynomesSIMON.c
@@ -36,4 +36,57 @@ void ajoutePolynomePolynome(Polynome *p, Polynome *q)
 	}
 }
 	
+void ajoutePolynomePolynome2(Polynome *p, Polynome *q)
+{
+	int i;
+	int e;
+	Monome monome_etape;
+	Polynome resultat;
 	
+	for (i = 0; i <= p->nb_monomes; ++i)
+	{
+		for(e = 0; i<= q->nb_monomes; i++)
+		{
+			if (p->tab_monomes[i].degre == q->tab_monomes[e].degre)
+			{
+				monome_etape.degre = p->tab_monomes[i].degre
+				monome_etape.coeff = p->tab_monomes[i].coeff + q->tab_monomes[e].coeff	
+				resultat->tab_monomes[i] = monome_etape	
+			}
+			else
+			{
+				resultat->tab_monomes[i] = p->tab_monomes[i] 
+			}
+		}
+		
+	}	
+}
+
+
+
+void multipliePolynomePolynome(Polynome *p, Polynome *q)
+{
+	Polynome resultat
+	if (p->nb_monomes > q->nb_monomes)
+	{
+		int stop=p->nb_monomes; //valeur d'arret
+		int i;
+		for(i=0;i<=stop;i++) // tant que le compteur est inférieur a la valeur d'arret
+		{
+			multiplieMonomePolynome(q->tab_monomes[i], p) ; 
+			ajoutePolynomePolynome (p, resultat); // on ajoute le monome d'indice i du polynome p au polynome p
+		}
+	}
+	else //si le nombre de monome de q est supérieur a celui de p
+	{
+		int stop=q->nb_monomes; //valeur d'arret
+		int i;
+		for ( i = 0; i <= stop; ++i) // tant que le compteur est inférieur a la valeur d'arret
+		{
+			multiplieMonomePolynome(p->tab_monomes[i], q);  
+			ajoutePolynomePolynome (q, resultat);
+		}
+
+	}
+
+}
